5da21f4453d3e9c8d8b3f10393a3e9dea02e7c3e
Jacob W. Greene <jacobwgreene@ufl.edu>
2017-02-12 16:00:53 -0500

    Redo

---
+++ footer.php
@@ -0,0 +1,21 @@
+<?php
+echo "      <footer>
+              <div class="wrap row small-up-1 medium-up-1">
+                <div class="column">
+                  <h4>Contact</h4>
+                  <hr>
+                  <a href="mailto:jacobwgreene@ufl.edu"><span>Email</span> jacobwgreene@ufl.edu</a>
+                  <div class="row">
+                    <div class="large-4 medium-5 columns">
+                      <div class="social-buttons">
+                          <a href="https://twitter.com/jacobwesgreene" target="_blank" class="icon-button twitter"><i class="fi-social-twitter"></i><span></span></a>
+                          <a href="http://www.augmentedwriting.org/" target="_blank" class="icon-button blogger"><i class="fi-social-blogger"></i><span></span></a>
+                          <a href="https://github.com/jacobwgreene" target="_blank" class="icon-button github"><i class="fi-social-github"></i><span></span></a> 
+                          <a href="https://www.linkedin.com/in/jake-greene-b3847b70?trk=hp-identity-name" target="_blank" class="icon-button linkedin"><i class="fi-social-linkedin"></i><span></span></a>              
+                      </div>
+                    </div>
+                  </div>
+                </div> 
+              </div>
+            </footer>";
+?>
